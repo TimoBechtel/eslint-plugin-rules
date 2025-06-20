48c2a974b195da584e934712a27f1178a15aa656
Timo Bechtel <48655057+TimoBechtel@users.noreply.github.com>
2025-06-20 14:41:55 +0200

    chore: add semantic-release config

---
+++ release.config.cjs
@@ -0,0 +1,19 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+	branches: [
+		'main',
+		'+([0-9])?(.{+([0-9]),x}).x',
+		{ name: 'beta', prerelease: true },
+		{ name: 'alpha', prerelease: true },
+	],
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/changelog',
+		'@semantic-release/npm',
+		'@semantic-release/github',
+		'@semantic-release/git',
+	],
+};
